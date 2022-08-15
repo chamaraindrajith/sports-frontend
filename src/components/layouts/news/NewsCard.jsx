a01541f460d0ecf5428cd3d3c19533a231ab39fd
z1nc0r3 <manujitha98@gmail.com>
2022-08-15 14:36:52 +0530

    fix : soccer news image

--- src/components/layouts/news/NewsCard.jsx
+++ src/components/layouts/news/NewsCard.jsx
@@ -2,11 +2,17 @@ import { React } from "react";
 import "./../../../css/NewsCrad.css";
 
 function NewsSideBar(props) {
+  let src = "https://sports.pfplapp.com/backend/public/data/news/images/espn_";
+  
+  if (props.sport == "cricket") {
+    src = "https://sports.pfplapp.com/backend/public/data/news/images/rapid_";
+  }
+
   return (
     <div className="stage_card">
       <div className="news_card_box">
         <div className="news_card_parent">
-          <img className="news_card_image" src={"https://sports.pfplapp.com/backend/public/data/news/images/rapid_" + props.news.image_id + ".jpeg"} id={"img_" + props.news.id}></img>
+            <img className="news_card_image" src={src + props.news.image_id + ".jpeg"} id={"img_" + props.news.id}></img>
           <span className="news_card_background">
             <h3 className="news_card_title">{props.news.title} </h3>
           </span>
