26823964ac5f44ee6dc50eeef37618bf05bbfc6d
chamaraindrajith <chamara.indrajith@outlook.com>
2022-11-24 10:25:24 +0530

    file server remove. CircularProgress added to menu

--- src/components/layouts/ExploreMenu.jsx
+++ src/components/layouts/ExploreMenu.jsx
@@ -1,15 +1,12 @@
 import { React, useState, useEffect } from "react";
 import { useLocation, Link } from "react-router-dom";
-// import './../../js/FileServer.js';
-import { saveAs } from 'file-saver';
+import CircularProgress from "@mui/material/CircularProgress";
 
 function ExploreMenu(props) {
   const [error, setError] = useState(null);
   const [isLoaded, setIsLoaded] = useState(false);
   const [menuitems, setMenuItems] = useState([]);
   const location = useLocation();
-
-  var FileSaver = require('file-saver');
 
   useEffect(() => {
     setIsLoaded(false);
@@ -37,27 +34,39 @@ function ExploreMenu(props) {
       );
   }
 
-  return (
-    <ul className="Ch">
-      {/* {console.log(menuitems)} */}
-      {menuitems.map((menuitem, key) => (
-        <Link to={"/" + props.sport + "/" + menuitem.slug} style={{ textDecoration: "none" }} className="menuitems" key={key}>
-          <li className="Fh">
-            <div className="ej">
-              {/* <img loading="lazy" className="flags" src={"https://static.livescore.com/i2/fh/" + menuitem.slug + ".jpg"} alt="" /> */}
-              <img loading="lazy" className="flags" src={"images/flags/" + menuitem.slug + ".jpg"} alt="" />
-              {/* {FileSaver.saveAs("https://static.livescore.com/i2/fh/" + menuitem.slug + ".jpg", menuitem.slug + ".jpg")} */}
-              {/* {FileSaver.saveAs("https://httpbin.org/image", "image.jpg")} */}
-              {}
-            </div>
-            <span className="menuItemName">
-              {menuitem.name}
-            </span>
-          </li>
-        </Link>
-      ))}
-    </ul>
-  );
+  if (error) {
+    return (
+      <div style={{ height: "800px" }}>
+        <div className="box_message">There are no news available</div>
+      </div>
+    );
+  } else if (!isLoaded) {
+    return (
+      <div style={{ height: "350px", display: "flex", justifyContent: "center" }}>
+        <div style={{ marginTop: "150px" }}>
+          <CircularProgress />
+        </div>
+      </div>
+    );
+  } else if (isLoaded) {
+    return (
+      <ul className="Ch">
+        {menuitems.map((menuitem, key) => (
+          <Link to={"/" + props.sport + "/" + menuitem.slug} style={{ textDecoration: "none" }} className="menuitems" key={key}>
+            <li className="Fh">
+              <div className="ej">
+                <img loading="lazy" className="flags" src={"images/flags/" + menuitem.slug + ".jpg"} alt="" />
+                { }
+              </div>
+              <span className="menuItemName">
+                {menuitem.name}
+              </span>
+            </li>
+          </Link>
+        ))}
+      </ul>
+    );
+  }
 }
 
 export default ExploreMenu;
