5e4eafb9721ea676c0fa8d74d6265acf92a028a1
chamaraindrajith <chamara.indrajith@outlook.com>
2022-12-03 00:40:58 +0530

    news.image_id

--- src/components/layouts/news/NewsCard.jsx
+++ src/components/layouts/news/NewsCard.jsx
@@ -2,21 +2,12 @@ import { React } from "react";
 import "./../../../css/NewsCrad.css";
 
 function NewsSideBar(props) {
-  var src = "";
-
-  if (props.sport == "cricket") {
-    src = "https://api.tvpool.net/data/news/images/rapid_";
-  } else if (props.sport == "soccer") {
-    src = "https://api.tvpool.net/data/news/images/espn_";
-  } else if (props.sport == "basketball") {
-    src = "https://api.tvpool.net/data/news/images/espn_";
-  }
 
   return (
     <div className="news_card">
       <div className="news_card_box">
         <div className="news_card_parent">
-          <img className="news_card_image" src={src + props.news.image_id + ".jpeg"} id={"img_" + props.news.id}></img>
+          <img className="news_card_image" src={"https://api.tvpool.net/data/news/images/" + props.news.api + props.news.image_id + ".jpeg"} id={"img_" + props.news.id}></img>
           <span className="news_card_background">
             <h3 className="news_card_title">{props.news.title} </h3>
           </span>
