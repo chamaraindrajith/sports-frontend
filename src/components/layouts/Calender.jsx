55ac41649e4845a70ea3a1b6dbde6f7282654b65
Lasith Manujitha <Manujitha98@gmail.com>
2023-01-12 22:59:34 +0530

    Add DateHandler logic

--- src/components/layouts/Calender.jsx
+++ src/components/layouts/Calender.jsx
@@ -1,5 +1,6 @@
 import { React } from "react";
 import Box from '@mui/material/Box';
+const date = new Date();
 
 function Calender() {
   return (
@@ -12,28 +13,29 @@ function Calender() {
             minHeight: "100%",
             width: "30px",
           }}
+          alt="live"
         />
       </div>
       <div id="dates">
         <div className="date">
-          <span>SAT</span>
-          <span className="date_number">25 JUL</span>
+          <span>{dayHandler(-2)}</span>
+          <span className="date_number">{dateHandler(-2)}</span>
         </div>
         <div className="date">
-          <span>SAT</span>
-          <span className="date_number">25 JUL</span>
+          <span>{dayHandler(-1)}</span>
+          <span className="date_number">{dateHandler(-1)}</span>
         </div>
         <div className="date">
           <span style={{color: "#ff6b00"}}>TODAY</span>
-          <span style={{color: "#ff6b00"}} className="date_number">25 JUL</span>
+          <span style={{color: "#ff6b00"}} className="date_number">{dateHandler(0)}</span>
         </div>
         <div className="date">
-          <span>SAT</span>
-          <span className="date_number">25 JUL</span>
+          <span>{dayHandler(1)}</span>
+          <span className="date_number">{dateHandler(1)}</span>
         </div>
         <div className="date">
-          <span>SAT</span>
-          <span className="date_number">25 JUL</span>
+          <span>{dayHandler(2)}</span>
+          <span className="date_number">{dateHandler(2)}</span>
         </div>
       </div>
       <img
@@ -42,11 +44,26 @@ function Calender() {
           minHeight: "100%",
           width: "25px",
         }}
+        alt="calendar"
       />
       
     </div>
     </Box>
   );
+}
+
+function dateHandler(rewind) {
+  let date = new Date();
+  date.setDate(date.getDate() + rewind);
+
+  return date.toUTCString().substring(5, 11).toUpperCase();
+}
+
+function dayHandler(rewind) {
+  let date = new Date();
+  date.setDate(date.getDate() + rewind);
+
+  return date.toUTCString().substring(0, 3).toUpperCase();
 }
 
 export default Calender;
