42d90043d2be416d386b1b52b884bf33814896b5
Chamara Indrajith <51538345+chamaraindrajith@users.noreply.github.com>
2023-04-17 17:40:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,3 +23,4 @@ if (process.env.NODE_ENV === "development") {
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+ 
